2d74a83b9fbd8e0378d8d89bef12d72133a6be1c
igorkovalskii <52951684+igorkovalskii@users.noreply.github.com>
2019-07-21 14:07:46 +0200

    parametra added

--- sandbox/src/main/java/ru/ki/test/First.java
+++ sandbox/src/main/java/ru/ki/test/First.java
@@ -5,6 +5,22 @@ import static java.lang.System.*;
 public class First {
 
   public static void main(String[] args) {
-    out.println("123456");
+    hello("world");
+    hello("user");
+    hello("admin");
+    double len = 5;
+    out.println("Площадь квадрата со стосроной " + len + " ровняется " + area(len) );
+    double a = 4;
+    double b = 6;
+    out.println("Площадь со стосронами " + a + " и " + b + " = " + area(a, b));
+  }
+  public static void  hello (String somebody){
+    out.println("Hello " + somebody + "!");
+  }
+  public static double area(double l){
+   return l*l;
+  }
+  public static double area (double a, double b){
+    return a*b;
   }
 }
